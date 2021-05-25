321a4beddaac7616e67744bde6a4d2dade447084
Levin <vbatallones.xvi@gmail.com>
2021-05-25 15:18:23 -0700

    I created my own logic for hiding the expenses form then I comment it out, to match what I am following on the react guide I also follow how he pass a function as a props as you can see in the cancel button

--- react-guide/src/components/NewExpense/ExpenseForm.js
+++ react-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 
 import './ExpenseForm.css';
+// import ToggleForm from './ToggleForm';
 
 const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState('');
 	const [enteredAmount, setEnteredAmount] = useState('');
 	const [enteredDate, setEnteredDate] = useState('');
+	// const [viewForm, setViewForm] = useState(false);
 
 	const titleChangeHandler = (e) => {
 		setEnteredTitle(e.target.value);
 	};
-	
+
 	const amountChangeHandler = (e) => {
 		setEnteredAmount(e.target.value);
 	};
@@ -31,43 +33,63 @@ const ExpenseForm = (props) => {
 		setEnteredTitle('');
 		setEnteredAmount('');
 		setEnteredDate('');
+		// setViewForm(false);
+		
 	};
+
+	// const toggleExpenseFormHandler = (e) => {
+	// 	e.preventDefault();
+	// 	!viewForm ? setViewForm(true) : setViewForm(false);
+	// };
 	return (
-		<form onSubmit={submitHandler}>
-			<div className="new-expense__controls">
-				<div className="new-expense__controls">
-					<label>Title</label>
-					<input
-						type="text"
-						value={enteredTitle}
-						onChange={titleChangeHandler}
-					/>
-				</div>
-				<div className="new-expense__controls">
-					<label>Amount</label>
-					<input
-						type="number"
-						min="0.01"
-						stpe="0.01"
-						value={enteredAmount}
-						onChange={amountChangeHandler}
-					/>
-				</div>
-				<div className="new-expense__controls">
-					<label>Date</label>
-					<input
-						type="date"
-						min="2019-01-01"
-						max="2022-12-31"
-						value={enteredDate}
-						onChange={dateChangeHandler}
-					/>
-				</div>
-			</div>
-			<div className="new-expense__actions">
-				<button type="submit">Add Expense</button>
-			</div>
-		</form>
+		// <div>
+		// 	{!viewForm ? (
+		// 		<button onClick={toggleExpenseFormHandler}>Add New Expense</button>
+		// 	) : (
+				<form onSubmit={submitHandler}>
+					<div className="new-expense__controls">
+						<div className="new-expense__controls">
+							<label>Title</label>
+							<input
+								type="text"
+								value={enteredTitle}
+								onChange={titleChangeHandler}
+							/>
+						</div>
+						<div className="new-expense__controls">
+							<label>Amount</label>
+							<input
+								type="number"
+								min="0.01"
+								stpe="0.01"
+								value={enteredAmount}
+								onChange={amountChangeHandler}
+							/>
+						</div>
+						<div className="new-expense__controls">
+							<label>Date</label>
+							<input
+								type="date"
+								min="2019-01-01"
+								max="2022-12-31"
+								value={enteredDate}
+								onChange={dateChangeHandler}
+							/>
+						</div>
+					</div>
+					<div className="new-expense__actions">
+						{/* <button
+							className="cancel-button"
+							onClick={toggleExpenseFormHandler}
+						>
+							Cancel
+						</button> */}
+						<button type="button" onClick={props.onCancel}>Cancel</button>
+						<button type="submit">Add Expense</button>
+					</div>
+				</form>
+			// )}
+		// </div>
 	);
 };
 
