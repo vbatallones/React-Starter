958cb185396393b4823caa7598535ee214caa2ec
Levin <vbatallones.xvi@gmail.com>
2021-05-25 15:19:26 -0700

    rendering a button, setting state, and create a conditional content for the form to wether the button is click

--- react-guide/src/components/NewExpense/NewExpense.js
+++ react-guide/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+	const [viewForm, setViewForm] = useState(false);
+
 	const saveExpenseData = (enteredExpenseData) => {
 		const expenseData = {
 			...enteredExpenseData,
 			id: Math.random().toString(),
 		};
 		props.onAddExpense(expenseData);
+		setViewForm(false)
 	};
+
+	const toggleExpenseFormHandler = () => setViewForm(true);
+
+	const cancelExpenseFormHandler = () => setViewForm(false);
 
 	return (
 		<div className="new-expense">
-			<ExpenseForm onSaveExpenseData={saveExpenseData} />
+			{!viewForm && (
+				<button onClick={toggleExpenseFormHandler}>Add New Expense</button>
+			)}
+			{viewForm && <ExpenseForm onSaveExpenseData={saveExpenseData} onCancel={cancelExpenseFormHandler} />}
 		</div>
 	);
 };
